bf6c834e3227490034918b04b52cc4271d094b3a
ahyangnb <zonggeu@gmail.com>
2020-10-13 14:16:43 +0800

    commit MyApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,32 +1,15 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:note_edit/note_edit.dart';
 import 'package:note_edit_example/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    try {
-      platformVersion = await NoteEdit.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
+      title: 'NoteEdit',
       home: new MyHomePage(),
     );
   }
